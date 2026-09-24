869186f0e0408f5617ac3ce17a11bf21214aeea4
slaterbastidas-cell <slaterbastidas@gmail.com>
2026-09-23 23:33:09 -0400

    Define region metadata

---
+++ world/region_data.gd
@@ -0,0 +1,9 @@
+class_name EclipseRegionData
+extends Resource
+
+@export var region_id: String
+@export var display_name: String
+@export var center: Vector3
+@export var radius_m: float = 12000.0
+@export var elevation_m: float = 0.0
+@export var biome_hint: String
