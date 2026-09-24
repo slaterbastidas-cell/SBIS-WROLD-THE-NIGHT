bc890f96f7d0f36292f7ee1799203d6615b6e01a
slaterbastidas-cell <slaterbastidas@gmail.com>
2026-09-23 23:15:05 -0400

    Define world-scale constants and official region names

---
+++ world/world_constants.gd
@@ -0,0 +1,21 @@
+class_name EclipseWorldConstants
+extends RefCounted
+
+const WORLD_SIZE_KM := 12.0
+const REGION_REFERENCE_AREA_KM2 := 450.0
+const REGION_REFERENCE_DIAMETER_KM := 21.0
+const WORLD_VERTICAL_RANGE_KM := 4.0
+const CHUNK_SIZE_M := 500.0
+const STREAM_RADIUS_CHUNKS := 2
+
+const REGION_NAMES := [
+	"Villa Eclipse",
+	"La Antigua Ciudadela",
+	"Cleopatt",
+	"Valle Desierto",
+	"Refugio de los Caídos",
+	"Isla de la Paz",
+	"La Biblioteca",
+	"El Cosmos",
+	"Castillo del Cosmos"
+]
